90a9e68b4e79d6482d33226d5bc6b5e0b131dc31
Marcos Jesus <marcosjesus.dev@gmail.com>
2023-05-11 03:13:31 -0300

    going up project

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script>
+import layout from './pages/index.vue'
+export default {
+  components: {
+    layout
+  }
+}
+</script>
+<template>
+  <div class="container w-full h-full p-0 m-0">
+    <layout />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-9xl">Template</h1>
+  </div>
+</template>
